c50d4a8ce8c13e7063a9ef8534bdf1010d12cb71
rithikrajkumar <rithik.rajkumar@icodelabs.co>
2025-10-15 22:14:32 +0530

    initial commit

---
+++ cmd/student-api/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a placeholder for the main function of the student API service.
+	fmt.Println("Student API service is running...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/rithikrajkumar4/student-backend-go
+
+go 1.25.1
